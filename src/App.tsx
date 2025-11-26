b1e7224df5058fcbe43b2a48b360deacce0c85ce
kevwasonga <kevinwasonga116@gmail.com>
2025-11-26 14:24:08 +0300

    Removed Contact section entirely

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Services from './components/Services';
-import Contact from './components/Contact';
+
 
 
 function App() {
@@ -157,7 +157,7 @@ function App() {
         <About />
         <Projects />
         <Services />
-        <Contact />
+
       </main>
 
 
