af002be9d1c35b6131c7e38acd5cf3053a864f92
kevwasonga <kevinwasonga116@gmail.com>
2025-12-09 11:48:04 +0300

    Update breakpoints for 6.5-inch mobile screens (360px base)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'xs': '480px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1440px',
-      '2xl': '1920px',
+      'xs': '360px',
+      'sm': '480px',
+      'md': '640px',
+      'lg': '768px',
+      'xl': '1024px',
+      '2xl': '1440px',
+      '3xl': '1920px',
     },
     extend: {
       fontFamily: {
